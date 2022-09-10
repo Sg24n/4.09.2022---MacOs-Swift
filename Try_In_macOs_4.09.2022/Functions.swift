415a0ce2c9fa892cc3f61de43213b79a250ba60b
Sg24n <47612337+Sg24n@users.noreply.github.com>
2022-09-10 08:02:20 +0200

    ok

---
+++ Try_In_macOs_4.09.2022/Functions.swift
@@ -0,0 +1,8 @@
+//
+//  Functions.swift
+//  Try_In_macOs_4.09.2022
+//
+//  Created by Kira Vaskovskiy on 7.09.
+//
+
+import Foundation
---
+++ Try_In_macOs_4.09.2022/View2.swift
@@ -0,0 +1,8 @@
+//
+//  View2.swift
+//  Try_In_macOs_4.09.2022
+//
+//  Created by Kira Vaskovskiy on 7.09.
+//
+
+import Foundation
